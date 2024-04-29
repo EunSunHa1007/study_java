dc9f8d86a7ad7a2d10196fd716e8aa1509cf6ebc
eunsunny <eunsunha2061@gmail.com>
2024-04-29 19:04:31 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+    //public static void main: 프로그램이 시작되는 곳 + 단축키 psvm
+    //System.out.prinln: 개발하면서 값을 출력해보는 기능 + sout
+
+    /* javac 명령어 : 자바로 코딩한 .java 파일을 자바 바이트코드로 번역(compile)
+       - javac Main.java
+       Main.class 파일 생성된것 확인
+
+       java 명령어를 통해 class 파일을 실행*/
+}
+
